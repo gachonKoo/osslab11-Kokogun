86daca32644d65a4eed62e879bb2acf7474ff719
Kokogun <koko21434250@gmail.com>
2024-11-16 23:36:16 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,4 +1,14 @@
 import geo.utils as utils
+__all__ = ['pythagoras' , 'circle']
+import math
+
+def pythagoras(a, b):
+  c = math.sqrt(a**2 + b**2)
+  return c
+
+def circle(r):
+  area = math.pi * r**2
+  return area
 
 # calculate the length of hypotenuse(c) a = 3 & b = 4
 a, b = 3, 4
